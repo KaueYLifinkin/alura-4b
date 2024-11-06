26c9d598d3216d20dba50ea38ed81a26986a92e8
KaueYLifinkin <152009280+KaueYLifinkin@users.noreply.github.com>
2024-11-06 10:39:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Volei',
+    'Quantos jogadores tem em um time de Volei?',
+    'Resposta:São 6 jogadores por time!'
+)
+criaCartao(
+    'Futebol',
+    'Quantos jogadores tem em um time deFutebol?',
+    'Resposta:São 11 jogadores por time!'
+)
+criaCartao(
+    'Basquete',
+    'Quantos jogadores tem em um time de Basquete?',
+    'Resposta:São 5 jogadores por time!'
+)
+criaCartao(
+    'Futebol Americano',
+    'Quantos jogadores tem em um time de Futebol Americano?',
+    'Cada equipe conta com 46 jogadores, mas apenas 11 de cada time estão em campo simultaneamente!'
+)
